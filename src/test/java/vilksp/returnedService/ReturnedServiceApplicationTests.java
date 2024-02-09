30b27c2a68222abcdf98e4efbe5a33b944bb36e3
vilksp <v.kasperavicius1@gmail.com>
2024-02-09 18:29:23 +0200

    initial commit

---
+++ src/test/java/vilksp/returnedService/ReturnedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vilksp.returnedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
