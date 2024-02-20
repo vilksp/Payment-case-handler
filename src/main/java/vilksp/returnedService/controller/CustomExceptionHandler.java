ec307cb47f8d487730489efbfed43cb5f93dbf7f
vilksp <v.kasperavicius1@gmail.com>
2024-02-20 16:35:56 +0200

    Unused annotation

--- src/main/java/vilksp/returnedService/controller/CustomExceptionHandler.java
+++ src/main/java/vilksp/returnedService/controller/CustomExceptionHandler.java
@@ -4,12 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import vilksp.returnedService.model.exception.CaseHandlerException;
 
-@RestController
+
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
